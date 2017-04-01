24d54c213ad334d0b73780529c88c4e10783dce8
Max von Hippel <maxvonhippel1996@gmail.com>
2017-04-01 12:53:48 -0700

    Untested.  Table fix + timestamp.
    Signed-off-by: Max von Hippel <maxvonhippel1996@gmail.com>

--- webapp/views.py
+++ webapp/views.py
@@ -2,7 +2,8 @@
 import socket
 from django.http import HttpResponse
 import sys
-
+import time
+import struct
 
 #from general import *
 sys.path.append("./webapp/")
@@ -33,10 +34,13 @@ s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 
 
 def index(request):
-    answer = []
+    answer = ''
+ 	answer.append(str('<head><style>table, th, td {border: 1px solid black;border-collapse: collapse;}</style></head><body><table><tr><th>Time Stamp</th><th>Destination MAC</th><th>Source MAC</th><th>Protocol</th><th>IP Version</th><th>IP Header Length</th><th>TTL</th><th>Protocol</th><th>Source IP</th><th>Target IP</th><th>Source Port</th><th>Destination Port</th><th>Length</th></tr>'))
     for raw_data in range(5):
         raw_data , addr = s.recvfrom(65565)
         pcap.write(raw_data)
+        unpacked = struct.unpack('@ I H H i I I I I I I I', raw_data[:40])
+        timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(unpacked))
         eth = Ethernet(raw_data)
         ipv4 = IPv4(eth.data)
         icmp = ICMP ( ipv4.data )
@@ -44,18 +48,9 @@ def index(request):
         udp = UDP (ipv4.data)
        # pcap =Pcap(pcap.data)
 
-        answer.extend ( [
-        		str('<head><style>table, th, td {border: 1px solid black;border-collapse: collapse;}</style></head><body><table>'),
-                str('<tr><th></th><th>Time Stamp:</th><th></th></tr>'.format()),
-                str('<tr><th>Destination MAC: {}</th><th>Source MAC: {}</th><th>Protocol: {}</th></tr>'.format ( eth.dest_mac ,eth.src_mac, eth.proto)),
-                str('<tr><th>IP Version: {}</th><th>IP Header Length: {}</th><th>TTL: {}</th></tr>'.format(ipv4.version, ipv4.header_length, ipv4.ttl)),
-                str('<tr><th>Protocol: {}</th><th>Source IP: {}</th><th>Target IP: {}</th></tr>'.format(ipv4.proto, ipv4.src, ipv4.target)),
-                str('<tr><th>Source Port: {}</th><th>Destination Port: {}</th><th></th></tr>'.format(tcp.src_port, tcp.dest_port)),
-                str('<tr><th>Source Port: {}</th><th>Destination Port: {}</th><th>Length: {}</th></tr>'.format(udp.src_port, udp.dest_port, udp.size)),
-                str('</table></body>')
-        ])
+        answer.append (str('<tr><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th></tr>'.format(timestamp, eth.dest_mac, eth.src_mac, eth.proto, ipv4.version, ipv4.header_length, ipv4.ttl, nipv4.proto, ipv4.src, ipv4.target, tcp.src_port, tcp.dest_port, udp.src_port, udp.dest_port, udp.size)))
 
-        answer.append (" ")
+    answer.append (str('</table></body>'))
 
     return HttpResponse(answer)
 
